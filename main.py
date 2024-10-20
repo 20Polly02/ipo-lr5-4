b5ae3bef6bf17825fd91130d1f69857b2d99b1d2
Polly Bel <polialekseevna80@gmail.com>
2024-10-20 18:16:28 +0300

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-str=input("Введите несколько слов :").lower()
-list_of_word = str.split()
-first_letter=[ letter[0] for letter in list_of_word]
-str2=''.join(first_letter)
-print(str2) 
+str=input("Введите несколько слов :").lower()#ввод слов с клавиатуры
+list_of_word = str.split()#создаем список из слов введенной строки
+first_letter=[ letter[0] for letter in list_of_word]#создаем список из первых букв каждого введенного слова
+str2=''.join(first_letter)#с помощью join() склеиваем первые буквы в одно слово
+print(str2)#вывод слова на консоль
